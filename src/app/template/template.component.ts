9095240b4e2382a827e9201762e5eca5c45f1db3
rasmiranjan.nayak <rasmiranjan.nayak@guavus.com>
2018-11-02 12:45:34 +0530

    added scss files

---
+++ src/app/template/template.component.ts
@@ -0,0 +1,18 @@
+import {
+  Component,
+  OnInit
+  } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.structure.scss']
+})
+export class TemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
